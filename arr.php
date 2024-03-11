7c3312cdfda9ffe6d666eb0f90a07501e62f38a8
habivan <xdolx@mail.ru>
2024-03-11 19:24:02 +0300

    меняем на php

---
+++ arr.php
@@ -0,0 +1,9 @@
+<?php 
+
+$articles = array(
+    array("author" => "John Doe", "date" => "2022-01-01", "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+    array("author" => "Jane Smith", "date" => "2022-01-05", "content" => "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+    array("author" => "Adam Johnson", "date" => "2022-01-10", "content" => "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."),
+    array("author" => "Eve Brown", "date" => "2022-01-15", "content" => "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+    array("author" => "Mike Williams", "date" => "2022-01-20", "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+);
--- pattern.php
+++ pattern.php
@@ -1,3 +1,4 @@
+<?php require_once 'arr.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -26,18 +27,19 @@
 			</nav>
 		</header>
 		<main>
-			<div class="card" style="width: 18rem">
-				<div class="card-body">
-					<h5 class="card-title">Card title</h5>
-					<h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-					<p class="card-text">
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</p>
-					<a href="#" class="card-link">Card link</a>
-					<a href="#" class="card-link">Another link</a>
+            <?php foreach($articles as $user):?>
+				<div class="card" style="width: 18rem m-5">
+					<div class="card-body">
+						<h5 class="card-title"><?=$user['author']?></h5>
+						<h6 class="card-subtitle mb-2 text-body-secondary"><?=$user['date']?></h6>
+						<p class="card-text">
+							<?=$user['content']?>
+						</p>
+						<a href="#" class="card-link">Card link</a>
+						<a href="#" class="card-link">Another link</a>
+					</div>
 				</div>
-			</div>
+			<?php endforeach;?>
 		</main>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 	</body>
