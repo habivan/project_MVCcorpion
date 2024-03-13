5e18ef64b5060bcab8f8d34ca20c55d54456a97e
habivan <xdolx@mail.ru>
2024-03-13 17:49:37 +0500

    создали роутер

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+define('ROOT', dirname(__DIR__));
+define('PUB', ROOT . '/public');
+define('CORE', ROOT . '/core');
+define('CONFIG', ROOT . '/config');
+define('APP', ROOT . '/app');
+define('CONTROLLERS', APP . '/controllers');
+define('VIEWS', APP . '/views');
+define('PATH', 'http://mvc');
